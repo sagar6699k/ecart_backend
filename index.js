32d1ceba2b9ed080edc20a4e4762f0ee9c51dcc8
Sagar <sagarkurewar@gmail.com>
2023-12-15 09:30:43 +0530

    adding changes

---
+++ index.js
@@ -0,0 +1,21 @@
+import express from "express";
+import connect from "./src/dbConfig/db.js";
+import { userRouter } from "./src/routes/user.route.js";
+
+
+const app = express();
+const PORT = 4000;
+app.use(express.json());
+//routes
+app.get("/", (req, res) => {
+    res.send("Express on Vercel");
+});
+app.use("/", userRouter)
+
+
+app.listen(PORT, async () => {
+    connect()
+    console.log(`Listening on the port:${PORT}`);
+})
+
+export { app };
